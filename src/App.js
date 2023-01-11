e13f7a831f54bcae2fc6b50d49f3c93fca38bc14
Apoka5555 <apoka5555@gmail.com>
2023-01-12 00:10:20 +0600

    Finished signUp page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import SignUpPage from "./pages/SignUpPage";
+import LanguageSelector from "./components/LanguageSelector";
+import { Route } from "react-router-dom";
 
 function App() {
-  return <SignUpPage />;
+  return (
+    <>
+      <div className="container pt-3">
+        <Route path="/signup" component={SignUpPage} />
+        <LanguageSelector />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,22 +1,122 @@
 import { Component } from "react";
+import axios from "axios";
+import { withTranslation } from "react-i18next";
+import Input from "../components/Input";
+import Alert from "../components/Alert";
+import ButtonWithProgress from "../components/ButtonWithProgress";
+import { signUp } from "../api/apiCalls";
 
 class SignUpPage extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    passwordRepeat: "",
+    apiProgress: false,
+    signUpSuccess: false,
+    errors: {},
+  };
+
+  onChange = (event) => {
+    const { id, value } = event.target;
+    const errorsCopy = { ...this.state.errors };
+    delete errorsCopy[id];
+    this.setState({
+      [id]: value,
+      errors: errorsCopy,
+    });
+  };
+
+  submit = async (event) => {
+    event.preventDefault();
+    const { username, email, password } = this.state;
+    const body = {
+      username,
+      email,
+      password,
+    };
+    this.setState({ apiProgress: true });
+    try {
+      await signUp(body);
+      this.setState({ signUpSuccess: true });
+    } catch (error) {
+      if (error.response.status === 400) {
+        this.setState({ errors: error.response.data.validationErrors });
+      }
+      this.setState({ apiProgress: false });
+    }
+  };
+
   render() {
+    const { t } = this.props;
+    let disabled = true;
+    const { password, passwordRepeat, apiProgress, signUpSuccess, errors } =
+      this.state;
+
+    if (password && passwordRepeat) {
+      disabled = password !== passwordRepeat;
+    }
+
+    let passwordMismatch =
+      password !== passwordRepeat ? t("passwordMismatchValidation") : "";
+
     return (
-      <div>
-        <h1>Sign Up</h1>
-        <label htmlFor="username">Username</label>
-        <input id="username" />
-        <label htmlFor="email">E-mail</label>
-        <input id="email" />
-        <label htmlFor="password">Password</label>
-        <input id="password" type="password" />
-        <label htmlFor="passwordRepeat">Password Repeat</label>
-        <input id="passwordRepeat" type="password" />
-        <button disabled>Sign Up</button>
+      <div
+        className="col-lg-6 offset-lg-3 col-md-8 offset-md-2"
+        data-testid="signup-page"
+      >
+        {!signUpSuccess && (
+          <form className="card" data-testid="form-sign-up">
+            <div className="card-header">
+              <h1 className="text-center">{t("signUp")}</h1>
+            </div>
+            <div className="card-body">
+              <Input
+                id="username"
+                label={t("username")}
+                onChange={this.onChange}
+                help={errors.username}
+              />
+              <Input
+                id="email"
+                label={t("email")}
+                onChange={this.onChange}
+                help={errors.email}
+              />
+              <Input
+                id="password"
+                label={t("password")}
+                onChange={this.onChange}
+                help={errors.password}
+                type="password"
+              />
+              <Input
+                id="passwordRepeat"
+                label={t("passwordRepeat")}
+                onChange={this.onChange}
+                help={passwordMismatch}
+                type="password"
+              />
+              <div className="text-center">
+                <ButtonWithProgress
+                  disabled={disabled}
+                  apiProgress={apiProgress}
+                  onClick={this.submit}
+                >
+                  {t("signUp")}
+                </ButtonWithProgress>
+              </div>
+            </div>
+          </form>
+        )}
+        {signUpSuccess && (
+          <Alert>Please check your e-mail to activate your account</Alert>
+        )}
       </div>
     );
   }
 }
 
-export default SignUpPage;
+const SignUpPageWithTranslation = withTranslation()(SignUpPage);
+
+export default SignUpPageWithTranslation;
