8e1c72417e71264c29210d253b8e0547c29f1859
Apoka5555 <apoka5555@gmail.com>
2023-01-18 00:00:13 +0600

    user page

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,6 +1,76 @@
 import { render, screen } from "./test/setup";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+import storage from "./state/storage";
+
+let logoutCount = 0;
+let header;
+const server = setupServer(
+  rest.post("/api/1.0/users/token/:token", (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+  rest.get("/api/1.0/users", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        content: [
+          {
+            id: 1,
+            username: "user-in-list",
+            email: "user-in-list@mail.com",
+            image: null,
+          },
+        ],
+        page: 0,
+        size: 0,
+        totalPages: 0,
+      })
+    );
+  }),
+  rest.get("/api/1.0/users/:id", (req, res, ctx) => {
+    header = req.headers.get("Authorization");
+    const id = Number.parseInt(req.params.id);
+    if (id === 1) {
+      return res(
+        ctx.json({
+          id: 1,
+          username: "user-in-list",
+          email: "user-in-list@mail.com",
+          image: null,
+        })
+      );
+    }
+    return res(
+      ctx.json({
+        id: id,
+        username: "user" + id,
+        email: "user" + id + "@mail.com",
+        image: null,
+      })
+    );
+  }),
+  rest.post("/api/1.0/auth", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ id: 5, username: "user5" }));
+  }),
+  rest.post("/api/1.0/logout", (req, res, ctx) => {
+    logoutCount += 1;
+    return res(ctx.status(200));
+  }),
+  rest.delete("/api/1.0/users/:id", (req, res, ctx) => {
+    return res(ctx.status(200));
+  })
+);
+
+beforeEach(() => {
+  logoutCount = 0;
+  server.resetHandlers();
+});
+
+beforeAll(() => server.listen());
+
+afterAll(() => server.close());
 
 const setup = (path) => {
   window.history.pushState({}, "", path);
@@ -85,5 +155,13 @@ describe("Routing", () => {
     const logo = screen.queryByAltText("Hoaxify");
     userEvent.click(logo);
     expect(screen.getByTestId("home-page")).toBeInTheDocument();
+  });
+
+  it("navigates to user page when clicking the username on user list", async () => {
+    setup("/");
+    const user = await screen.findByText("user-in-list");
+    userEvent.click(user);
+    const page = await screen.findByTestId("user-page");
+    expect(page).toBeInTheDocument();
   });
 });
